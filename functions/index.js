6d003c2fe145bde8ddc2dd6cdaee7966de758ef0
孤独のコーヒー <64074181+solitary-coffee@users.noreply.github.com>
2026-09-05 11:56:58 +0900

    Load exam 47 preliminary answer frame

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,35 @@
-const RESPONSIVE_STYLESHEETS = [
+const PAGE_STYLESHEETS = [
   '<link rel="stylesheet" href="/assets/mobile.css?v=20260904" media="all">',
-  '<link rel="stylesheet" href="/assets/header-stability.css?v=20260904" media="all">'
+  '<link rel="stylesheet" href="/assets/header-stability.css?v=20260904" media="all">',
+  '<link rel="stylesheet" href="/assets/exam47-preliminary.css?v=20260905" media="all">'
 ].join('\n  ');
 
-function withResponsiveStyles(html) {
-  if (html.includes('/assets/mobile.css') && html.includes('/assets/header-stability.css')) return html;
-  if (html.includes('</head>')) {
-    return html.replace('</head>', `  ${RESPONSIVE_STYLESHEETS}\n</head>`);
+const PAGE_SCRIPTS = [
+  '<script defer src="/assets/exam47-preliminary.js?v=20260905"></script>'
+].join('\n  ');
+
+function withPageEnhancements(html) {
+  let output = html;
+
+  if (!output.includes('/assets/mobile.css') ||
+      !output.includes('/assets/header-stability.css') ||
+      !output.includes('/assets/exam47-preliminary.css')) {
+    if (output.includes('</head>')) {
+      output = output.replace('</head>', `  ${PAGE_STYLESHEETS}\n</head>`);
+    } else {
+      output = `${PAGE_STYLESHEETS}\n${output}`;
+    }
   }
-  return `${RESPONSIVE_STYLESHEETS}\n${html}`;
+
+  if (!output.includes('/assets/exam47-preliminary.js')) {
+    if (output.includes('</body>')) {
+      output = output.replace('</body>', `  ${PAGE_SCRIPTS}\n</body>`);
+    } else {
+      output = `${output}\n${PAGE_SCRIPTS}`;
+    }
+  }
+
+  return output;
 }
 
 export async function onRequestGet(context) {
@@ -28,7 +49,7 @@ export async function onRequestGet(context) {
   headers.delete('etag');
   headers.set('content-type', 'text/html; charset=UTF-8');
 
-  return new Response(withResponsiveStyles(html), {
+  return new Response(withPageEnhancements(html), {
     status: asset.status,
     statusText: asset.statusText,
     headers
